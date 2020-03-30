51937b9f8193fb8802ffb31658420fbc6da56c1d
Mathtzt <51196790+Mathtzt@users.noreply.github.com>
2020-03-30 14:28:43 -0300

    removend commands.js e passando cod para usuarioHelper

---
+++ cypress/integration/common/helpers/usuarioHelper.js
@@ -0,0 +1,15 @@
+export const UsuarioHelper = {
+    MASTER_INTERNO: {
+        "cnpj": "00622416000141",
+        "nome": "teste",
+        "senha": "Padaria123",
+    },
+
+    login: function () {
+        cy.visit('portal-h.bndes.gov.br/habilitacao')
+        cy.get(loc.LOGIN.CNPJOUCPF).type(this.MASTER_INTERNO.cnpj)
+        cy.get(loc.LOGIN.USUARIO).type(this.MASTER_INTERNO.nome)
+        cy.get(loc.LOGIN.SENHA).type(this.MASTER_INTERNO.senha)
+        cy.get(loc.LOGIN.BTN_ENTRAR).click()
+    }
+}
--- cypress/integration/common/helpers/usuarioHerlper.js
+++
@@ -1,7 +0,0 @@
-export const UsuarioHelper = {
-    MASTER_INTERNO: {
-        "cnpj": "00622416000141",
-        "nome": "teste",
-        "senha": "Padaria123",
-    }
-}
