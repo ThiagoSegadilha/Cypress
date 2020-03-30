fa2e45c49049e57a29b631dce501ad99e5da08e5
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2020-03-30 13:58:41 -0300

    criacao do usuarioHelper

---
+++ cypress/integration/common/helpers/usuarioHerlper.js
@@ -0,0 +1,7 @@
+export const UsuarioHelper = {
+    MASTER_INTERNO: {
+        "cnpj": "00622416000141",
+        "nome": "teste",
+        "senha": "Padaria123",
+    }
+}
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,4 +16,4 @@ module.exports = (on, config) => {
     on('file:preprocessor', cucumber());
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
