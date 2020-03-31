8a916fda677f8a5771e1b5ada447d69c7c1d427a
Mathtzt <51196790+Mathtzt@users.noreply.github.com>
2020-03-31 14:34:51 -0300

    Correção nos testes e diretorios usados para padronizar o projeto

--- cypress/integration/common/financiamento.steps.js
+++ cypress/integration/common/financiamento.steps.js
@@ -1,14 +1,25 @@
 import {When} from "cypress-cucumber-preprocessor/steps";
+import loc from "../../support/locators";
+
 
 When(/^clico no icone de financiamentos$/, () => {
     cy.get('.panel > #bn-navbar > .nav > :nth-child(2) > .nav-link > .d-flex > .ng-tns-c5-2 > .fa')
         .click()
 })
 
-When(/^E vejo a tela de financiamento$/, () => {
+When(/^vejo a tela de financiamento$/, () => {
     cy.get('h3').contains('Solicitações de Financiamentos')
 })
 
 When(/^clico no botão de solicitar financiamento$/, () => {
     cy.get('[data-testid=solicitarFinanciamento]').click()
+})
+
+When (/clico no botão salvar/, () => {
+    cy.get('[data-testid=salvar]').click()
+})
+
+
+Then (/vejo botão de enviar habilitado/, () => {
+    cy.get(loc.FORM_FINANCIAMENTO.BTN_ENVIAR).should("not.have.css", "disabled")  
 })
