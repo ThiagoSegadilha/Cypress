7bbd21c78b6c35f875f36d788a275ec9d55a3841
Mathtzt <51196790+Mathtzt@users.noreply.github.com>
2020-03-31 14:02:42 -0300

    Criação de cucumber e preenchimento do form de financiamento - Cenário 2

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -4,6 +4,9 @@ const locators = {
         USUARIO: '[data-testid=login-usuario]',
         SENHA: '[data-testid=login-senha]',
         BTN_ENTRAR: '[data-testid=entrar]'
+    },
+    FORM_FINANCIAMENTO: {
+        BTN_ENVIAR: '.d-flex > botao-workflow-processo > botao-workflow.ng-star-inserted > .ng-star-inserted > .btn'
     }
 }
 export default locators;
