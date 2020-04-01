e4ea95ac376fbe5aaff6025ebdbc2e675f932010
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2020-04-01 16:02:24 -0300

    iniciando o teste cypress do cenario 4

--- cypress/integration/Cenarios/Cenario4/cenario4.steps.js
+++ cypress/integration/Cenarios/Cenario4/cenario4.steps.js
@@ -1,1 +1,5 @@
 import {When, Given, Then} from "cypress-cucumber-preprocessor/steps";
+
+Given(/^que abri uma solicitação em análse$/, () => {
+    cy.get('[data-testid=nomeEstadoAtual]').contains("em análise").last().click()
+})
